94074a4044c5866edb22aa5fe0ebbd0b1d0eb8a8
A01636355 <moringos7@gmail.com>
2021-02-17 02:23:08 +0000

    Cambios en HelloWorld

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+//Compila con: gcc <file.c> -o <file exe>
 
-int main(){
-	printf("Hola a todos :D\n");
+int main(int numarg, char **argv){
+	//El uno es por las palabras en el comando
+	printf("Hola: ");
+	for(int i  = 1; i < numarg; i++){
+		printf("%s ",argv[i]);	
+	}
+	printf("\n");
 	return 0;
 }
