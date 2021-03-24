ff780914d6c64f89357a8cb52b96232bb509e07f
Moringos7 <moringos7@gmail.com>
2021-03-24 03:18:11 +0000

    Practica 5 2.0

--- IPC/cuentaLineas.c
+++ IPC/cuentaLineas.c
@@ -4,15 +4,19 @@
 
 int main(){
     char c;
+    char isIn = 0;
     int cont = 0;
     while(read(0,&c,1)){
+        isIn = 1;
         if(c == '\n'){
             cont += 1;
         }
     }
-    if(c != '\n'){
-        cont += 1;
-    }
+    if(isIn){
+        if(c != '\n'){
+            cont += 1;
+        }
+    }    
     printf("%d \n",cont);
     return 0;
 }
